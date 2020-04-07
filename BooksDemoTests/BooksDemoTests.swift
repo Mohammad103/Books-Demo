e569248b27a6eb3b37677e082044a5a45ad314fe
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2020-04-07 07:57:23 +0200

    Initial Commit

---
+++ BooksDemoTests/BooksDemoTests.swift
@@ -0,0 +1,34 @@
+//
+//  BooksDemoTests.swift
+//  BooksDemoTests
+//
+//  Created by Mohammad Shaker on 4/7/20.
+//  Copyright © 2020 Mohammad Shaker. All rights reserved.
+//
+
+import XCTest
+@testable import BooksDemo
+
+class BooksDemoTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
