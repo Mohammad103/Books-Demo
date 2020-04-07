3cd38f6b147bb8f193f8159c42110613cf881031
Mohammad Shaker <mohammad.shaker.103@gmail.com>
2020-04-07 08:22:08 +0200

    ADD: Splash screen

---
+++ BooksDemo/Common/Extensions/UIColorExtension.swift
@@ -0,0 +1,13 @@
+//
+//  UIColorExtension.swift
+//  BooksDemo
+//
+//  Created by Mohammad Shaker on 4/7/20.
+//  Copyright © 2020 Mohammad Shaker. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    static let defaultAppColor = #colorLiteral(red: 1, green: 0.3333333333, blue: 0, alpha: 1)
+}
